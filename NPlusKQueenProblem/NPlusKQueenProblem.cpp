2b92b542f993fd960b939bfbc4c60abd2f6cbf75
francisco.amorim <francisco.amorim@iugome.com>
2021-08-16 19:34:09 -0700

    Get input from cin
    Added code to ask for input and read input.

--- NPlusKQueenProblem/NPlusKQueenProblem.cpp
+++ NPlusKQueenProblem/NPlusKQueenProblem.cpp
@@ -7,6 +7,34 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	cout << "Welcome to the N + K Queen Problem solver." << endl;
+	cout << "To calculate the number of solutions for the N + K Queen Problem first enter the size of the board (N): ";
+	int boardSize;
+
+	cin >> boardSize;
+	while (cin.fail() || boardSize < 1)
+	{
+		cout << "Invalid input for board size (N). Enter a valid number: ";
+		cin.clear();
+		cin.ignore(1i64, '\n');
+		cin >> boardSize;
+	}
+	
+	cout << endl;
+
+	cout << "Enter the number of pawns (K):";
+	int pawns;
+	cin >> pawns;
+	while (cin.fail() || pawns < 0)
+	{
+		cout << "Invalid input for number of pawns (K). Enter a valid number: ";
+		cin.clear();
+		cin.ignore(1i64, '\n');
+		cin >> pawns;
+	}
+	cout << endl;
+
+	cout << "Looking for solutions to put " << (boardSize + pawns) << " queens in a " << boardSize << "x" << boardSize << " board with " << pawns << " pawns..." << endl;
+
 	return 0;
 }
